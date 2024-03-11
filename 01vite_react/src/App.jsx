e678c7f4a7ad55fecf551ec85a14bc35a9e3f4b2
mriduldutt <mirdul12dutt@gmail.com>
2024-03-11 22:21:16 +0530

    "commit"

--- 01vite_react/src/App.jsx
+++ 01vite_react/src/App.jsx
@@ -1,14 +1,39 @@
-// import { useState } from 'react'
+import React,{ useState } from 'react';
 // import './App.css'
 import Chai from './chai.jsx'
 
 function App() {
 
+  let [counter,setCounter] = useState(5);
+
+  // let counter = 5;
+
+  const addValue = () => {
+    // counter++;
+    console.log(`Value Added `, counter);
+    if(counter >= 20){
+      counter = 19;
+    }
+    setCounter(counter+1);
+  
+  }
+
+  const removeValue = () => {
+    // counter--;
+    if(counter <= 0){
+      counter = 1;
+    }
+    setCounter(counter-1);
+  }
 
   return (
     <>
-      <Chai/>
-      <h1>Chai React</h1>
+       <h1>Chai & React</h1>
+       <h2>Counter Value : {counter}</h2>
+       <button onClick={addValue}>Add Value {counter}</button>
+       <br /><br />
+       <button onClick={removeValue}>Remove Value {counter}</button>
+       <p>footer : {counter}</p>
     </>
   )
 }
