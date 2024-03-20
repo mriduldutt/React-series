5b956d8a47a301e0b922cf39eada06415091b6ed
mriduldutt <mirdul12dutt@gmail.com>
2024-03-20 17:25:29 +0530

    "sac"

---
+++ 06_CustomHooks/currencyConvertor/src/components/InputBox.jsx
@@ -0,0 +1,49 @@
+// import React,{useId} from "react";
+
+// export default function InputBox({
+//   label,
+//   amount,
+//   onAmtChange,
+//   onCurrencyChange,
+//   currencyOptions = [],
+//   selectCurrency = "usd",
+//   amountDisable = false,
+//   CurrencyDisable = false,
+//   className = "",
+// }) {
+
+//     //useID Hooks
+//     const amountInputid = useId();
+  
+//   return (
+//     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+//       <div className="w-1/2">
+//         <label htmlFor={amountInputid} className="text-black/40 mb-2 inline-block">{label}</label>
+//         <input id={amountInputid}
+//           className="outline-none w-full bg-transparent py-1.5"
+//           type="number"
+//           placeholder="Amount" disabled={amountDisable} value={amount} onChange={(e)=>{
+//             onAmtChange && onAmtChange(Number(e.target.value))
+//           }}
+//         />
+//       </div>
+//       <div className="w-1/2 flex flex-wrap justify-end text-right">
+//         <p className="text-black/40 mb-2 w-full">Currency Type</p>
+//         <select className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none" value={selectCurrency} 
+//         onChange={(e)=>{onCurrencyChange && onCurrencyChange(e.target.value)}} disabled={CurrencyDisable}>
+        
+//           {currencyOptions.map((option) => (
+//               <option key={option} value={option}>
+//                 {option}</option>
+//           ))}
+        
+//         </select>
+//       </div>
+//     </div>
+//   );
+// }
+
+
+
+
+
---
+++ 06_CustomHooks1/currenyConvertor/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useState } from 'react'
+import InputBox from './components/InputBox'
+import { useCurrencyInfo } from './customHooks/useCurrencyInfo'
+
+
+function App() {
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+
+  const options = Object.keys(currencyInfo)
+
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
+  
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to])
+  }
+
+  return (
+    <div>
+    <h1>Me 2</h1>
+    </div>
+);
+}
+
+export default App;
